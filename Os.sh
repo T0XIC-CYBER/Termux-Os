a86fff33e14d3f119da9af5da8df7fdcc1e12664
TOXIC CYBER SECURITY <104613340+T0XIC-CYBER@users.noreply.github.com>
2023-01-12 08:25:30 +0600

    Add files via upload

---
+++ Os.sh
@@ -0,0 +1,143 @@
+clear
+echo -e "\e[31m ________    _____   ______       __    __     __    __   __     __  "
+echo -e "\e[31m(___  ___)  / ___/  (   __ \      \ \  / /     ) )  ( (  (_ \   / _) "
+echo -e "\e[96m    ) )    ( (__     ) (__) )     () \/ ()    ( (    ) )   \ \_/ /   "
+echo -e "\e[95m   ( (      ) __)   (    __/      / _  _ \     ) )  ( (     \   /   "
+echo -e "\e[93m    ) )    ( (       ) \ \  _    / / \/ \ \   ( (    ) )    / _ \    "
+echo -e "\e[94m   ( (      \ \___  ( ( \ \_))  /_/      \_\   ) \__/ (   _/ / \ \_  "
+echo -e "\e[92m   /__\      \____\  )_) \__/  (/          \)  \______/  (__/   \__)"
+
+
+echo -e " \e[91m                                                   ____      _____  "
+echo -e " \e[91m                                                  / __ \    / ____\ "
+echo -e " \e[96m                                                 / /  \ \  ( (___   "
+echo -e " \e[96m                                                ( ()  () )  \___ \  "
+echo -e " \e[1;91m FACEBOOK\e[96m / \e[100;97mFACEBOOK.COM/ARIF_HASNAIN \e[0;31m          ( ()  () )      ) )"
+echo -e " \e[1;91m GitHub\e[96m /\e[1;93m TOXIC_CYBER                            \ \__/ /   ___/ /"
+echo -e " \e[1;91m Telegram\e[1;96m /\e[1;92m TOXIC_CYBER                           \____/   /____/"
+echo ""
+
+banner ( ) {
+                  
+                  
+                  printf ""
+                  echo -e "\e[1;31m  [\e[32m✅\e[31m] \e[1;91m DEVELOPER BY:  \e[1;36mARIF HASNAIN\e[93m/ \e[100;92m FACEBOOK.COM/ARIF HASNAIN \e[0m"
+                  }
+
+                  wr  ( )  {
+                               printf " [ ! ] INVALID INPUT !!!
+"| lolcat
+                               selection
+                               }
+                               1line() {
+                                                         apt update && apt upgrade
+                                                         pkg install zsh -y
+                                                         pkg install git -y
+                                                         pkg install figlet toilet -y
+                                                         pkg install ruby  -y
+                                                         pkg install wget  -y
+                                                       
+                                                         gem install lolcat 
+                                                         pkg install curl -y
+                                                         pkg install zsh -y
+                                                         
+                                                         clear
+                                                         cd ~/Termux-Os/.object/ && cp -r 'ANSI Shadow.flf'  $PREFIX/share/figlet/ASCII-Shadow.flf 
+                                                         git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+                                                       
+                                                         pkg install toilet figlet exa -y
+                                                         cd ~/Termux-Os/.object
+                                                         rm -rf ~/.termux/colors.properties
+                                                         rm -rf /data/data/com.termux/files/usr/etc/motd
+                                                         cp -r .colors.properties ~/.termux/colors.properties
+                                                         cp -r .termux.properties ~/.termux/termux.properties
+                                                         clear
+                                                         cd ~/Termux-Os ; bash Os.sh
+                                                         
+                                                         }
+                                                         2line() {
+                                                                               rm -rf ~/.zshrc
+                                                                               git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+                                                                               cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+                                                                               cd ~/Termux-Os ; bash Os.sh
+                                                                                }
+                                                                               3line() {
+                                                                                                 pkg install zsh
+                                                                                                 chsh -s zsh
+                                                                                                 cd ~/Termux-Os ; bash Os.sh
+                                                                                                  }
+                                                                                                   4line() {
+                                                                                                                         chsh -s bash
+                                                                                                                         cd  ~/Termux-Os ; bash Os.sh
+                                                                                                                         }
+                                                                                                                         5line() {
+                                                                                                                                              rm -rf ~/.zshrc
+                                                                                                                                              cd ~/Termux-Os/.object
+                                                                                                                                              bash  .2.sh
+                                                                                                                                              clear ; cd ~/Termux-Os ; bash Os.sh
+                                                                                                                                                  }
+                                                                                                                                                 
+                                                                                                                                                6line() {
+                                                                                                                                                                         cd ~/Termux-Os/.object
+                                                                                                                                                                         bash .1.sh
+                                                                                                                                                                         clear ; cd ~/Termux-Os ; bash Os.sh
+                                                                                                                                                                       }
+                                                                                                                                                                       7line() {
+                                                                                                                                                                                                                    cd ~/Termux-Os/.object
+                                                                                                                                                                                                                    rm -rf ~/.zshrc
+                                                                                                                                                                                                                    chsh -s zsh
+                                                                                                                                                                                                                    bash .3.sh
+                                                                                                                                                                                                                    clear ; cd ~/Termux-Os ; bash Os.sh
+                                                                                                                                                                                                                     }
+                                                                                                                                                                                                                     8line() {
+                                                                                                                                                                                                                                  rm -rf ~/Termux-Os
+                                                                                                                                                                                                                                  cd
+                                                                                                                                                                                                                                  git clone https://github.com/T0XIC-CYBER/Termux-Os
+                                                                                                                                                                                                                                  cd ~/Termux-Os ; bash Os.sh
+                                                                                                                                                                                                                                  }
+    
+                                 selection () {
+                                            cd ~/Termux-Os
+                                            echo -e -n "\e[1;96m CHOOSE AN \e[1;96m OPTION : \e[0m"| lolcat
+                                            read a
+                                            case $a in
+                                            1) 1line ;;
+                                            2) 2line ;;
+                                            3) 3line ;;
+                                            4) 4line ;;
+                                            5) 5line ;;
+                                            6) 6line ;;
+                                            7) 7line ;;
+                                            8) 8line ;;
+                                            9) exit ;;
+                                            *) wr ;;
+                                            esac
+                                            }
+
+                  menu () {
+                                  banner
+                                  printf "\n\n[ 1 ] NECESSARY SET-UP
+" | lolcat
+                                  printf "[ 2 ] ZSH SET-UP
+" | lolcat
+                                  printf "[ 3 ] ZSH SHELL
+"| lolcat
+                                  printf "[ 4 ] BASH SHELL
+"| lolcat
+                                  printf "[ 5 ] ZSH BANNER
+"| lolcat
+                                  printf "[ 6 ] ZSH THEME
+"| lolcat
+                                  printf "[ 7 ] HIGHLIGHT / AutoSuggest
+"| lolcat
+                                  printf "[ 8 ] UPDATE
+"| lolcat
+                                  printf "[ 9 ] EXIT
+
+
+
+"| lolcat
+                                  
+                                  selection
+                                  }
+                  menu
